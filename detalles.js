0483874c944b0df55ac52db42ca2dff4821dd940
TeddyRichard <teddyheredia@gmail.com>
2023-09-30 22:36:16 -0300

    Trabajo Final TP-2

---
+++ detalles.js
@@ -0,0 +1,28 @@
+
+
+const destacado = JSON.parse(localStorage.getItem("producto"));
+
+
+const mostrarProducto = document.querySelector("#producto-elegido");
+ 
+
+destacado.forEach((productos) => 
+    {
+    let content = document.createElement("div");
+    content.className = "card2";
+    content.innerHTML = `
+    <div class"item">
+        <figure>
+          <img src="${productos.imagen}">
+        <figure>
+        <div class="info-product">
+          <p class="marca">${productos.marca}</p>
+          <p class="modelo">${productos.modelo}</>
+          <p class="detalle">${productos.detalle}</p>
+          <p class="price">${productos.precio}</p>
+          <p class="puntos">${productos.puntaje}</p>
+        </div> 
+    </div> `;
+
+    mostrarProducto.append(content);
+    })
---
+++ script.js
@@ -0,0 +1,84 @@
+
+const listaDeProductos = document.querySelector("#product-list");
+  
+let productos;
+const cargarProductos = async () =>
+{
+ 
+ productos = localStorage.getItem("productos");
+
+    if(productos == null)
+    {
+
+      const response = await fetch("productos.json");
+      productos = await response.json();
+
+      localStorage.setItem("productos", JSON.stringify(productos));
+
+    }
+    
+    if (typeof productos == "string")
+     {
+        productos = JSON.parse(productos);
+     }
+
+    
+};
+    
+
+cargarProductos();
+
+
+productos.forEach((productos) => 
+{
+  let content = document.createElement("div");
+  content.className = "card";
+  content.innerHTML = `
+  <div class="item">
+  <figure>
+      <img src="${productos.img}">
+  </figure>
+    <div class="info-product">
+      <p class="marca">${productos.marca}</p>
+      <p class="price">${productos.precio}</p>
+    </div>
+  </div>`;
+
+  listaDeProductos.append(content);
+
+  let masDetalles = document.createElement("button");
+  masDetalles.innerText = "Más Detalles";
+  content.append(masDetalles);
+  masDetalles.className = "info-product";
+
+
+  const productoElegido = [];
+
+  masDetalles.addEventListener("click", () =>
+   {
+    
+    productoElegido.push({
+      id: productos.id,
+      imagen: productos.img,
+      marca: productos.marca,
+      modelo: productos.modelo,
+      detalle: productos.detalle,
+      precio: productos.precio,
+      puntaje: productos.puntuacion,
+    });
+    
+    localStorage.setItem("producto", JSON.stringify(productoElegido));
+ 
+    location.href ="detalles.html";
+  })
+
+});
+
+
+
+
+
+
+
+  
+
